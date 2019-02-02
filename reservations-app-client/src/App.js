b3b26a2dbfc75dd579cdf3a62a78a7e4465aaa49
Matheus Marotti <matheus.marotti@injunior.com.br>
2019-02-02 12:05:35 -0200

    Login, não guarda sessão

--- reservations-app-client/src/App.js
+++ reservations-app-client/src/App.js
@@ -1,12 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
 import './App.css';
 import Routes from "./Routes";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+  }
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
     return (
-      <div className="App container">
-        <Routes />
+        <div className="App container">
+        {this.state.isAuthenticated
+            ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+            : <Fragment>
+                  <NavItem href="/logout">Signup</NavItem>
+                  <NavItem href="/login">Login</NavItem>
+              </Fragment>
+          }
+
+        <Routes childProps={childProps} />
         <footer>
   				<img src="img/white-logo.svg" alt="Logo branca dos Minions"/>
   				<p>Copyright © Loja dos Minions 2019</p>
@@ -15,5 +45,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
