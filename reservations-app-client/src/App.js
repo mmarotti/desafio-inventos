df146e6ef66bbc532d85fefd5c3be6c1fd74ff80
Matheus Marotti <matheus.marotti@injunior.com.br>
2019-02-02 12:13:00 -0200

    Guardando sessão login

--- reservations-app-client/src/App.js
+++ reservations-app-client/src/App.js
@@ -3,21 +3,39 @@ import './App.css';
 import Routes from "./Routes";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Auth } from "aws-amplify";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      isAuthenticating: true
     };
+  }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    this.setState({ isAuthenticating: false });
   }
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
   }
 
-  handleLogout = event => {
+  handleLogout = async event => {
+    await Auth.signOut();
+
     this.userHasAuthenticated(false);
   }
 
@@ -27,6 +45,7 @@ class App extends Component {
       userHasAuthenticated: this.userHasAuthenticated
     };
     return (
+        !this.state.isAuthenticating &&
         <div className="App container">
         {this.state.isAuthenticated
             ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
